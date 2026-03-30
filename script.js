7e91290f3e1a3df16df97b4ae4a5f5b1b871ab99
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 11:10:24 -0300

    feat: Uso de parse, console e alert

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = prompt('Digite seu nome:')
+
+console.log('Bem Vindo!');
+// console.log('Olá, ' + nome)
+console.log(`Olá ${nome}!`)
+
+alert(`Bem-vindo, ${nome}`)
+
+let idade = parseInt(prompt('Digite sua idade: '))
+// Number(prompt('mensagem'))
+// let idadeInt = parseInt(idade);
+// idade = parseInt(idade)
+alert(`Legal, você terá ${idade + 25 } anos daqui a 25 anos!`)
